f8ca668fecdf66391b5c9031a7ef30d5b8161d36
cstmdzx <cstmdzx@163.com>
2019-09-07 01:17:22 +0800

    AppMain.vue

---
+++ src/views/components/AppMain.vue
@@ -0,0 +1,18 @@
+<template>
+   <section>
+       <transition name="fade" mode="out-in">
+           <router-view></router-view>
+       </transition>
+   </section>
+</template>
+
+<script>
+    export default {
+        name:'AppMain'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
